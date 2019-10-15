23e5f6d34e38a145931b6c71c114a096d05abac6
ray-bang8 <student@ALEM-317038.alem.school>
2019-10-16 03:57:22 +0600

    ikima

---
+++ isnegative.go
@@ -0,0 +1,23 @@
+package main 
+
+import "github.com/01-edu/z01"
+
+func IsNegative(nb int) rune {
+
+    if nb >= 0 {
+        return 'F'
+    } else {
+        return 'T'
+    }
+}
+
+func main() {
+
+    z01.PrintRune(IsNegative(1))
+	z01.PrintRune('\n')
+    z01.PrintRune(IsNegative(0))
+    z01.PrintRune('\n')
+    z01.PrintRune(IsNegative(-1))
+    z01.PrintRune('\n')
+
+}
