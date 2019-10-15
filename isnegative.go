33ba0fbf8574bd0bc0c611ac68c76776000cc813
ray-bang8 <student@ALEM-317038.alem.school>
2019-10-16 04:10:51 +0600

    ikima

--- isnegative.go
+++ isnegative.go
@@ -4,9 +4,9 @@ import "github.com/01-edu/z01"
 
 func IsNegative(nb int) rune {
     if nb >= 0 {
-        return 'F'
-    } else {
-        return 'T'
+        	return 'F'
+	} else {
+        	return 'T'
     }
 }
 
@@ -14,9 +14,9 @@ func main() {
 
 	z01.PrintRune(IsNegative(1))
 	z01.PrintRune('\n')
-    z01.PrintRune(IsNegative(0))
-    z01.PrintRune('\n')
-    z01.PrintRune(IsNegative(-1))
-    z01.PrintRune('\n')
-
+	z01.PrintRune(IsNegative(0))
+	z01.PrintRune('\n')
+	z01.PrintRune(IsNegative(-1))
+	z01.PrintRune('\n')
+	
 }
