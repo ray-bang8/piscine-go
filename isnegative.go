029f20334d28903ca4b378188dcff10f61c16887
ray-bang8 <student@ALEM-317038.alem.school>
2019-10-16 04:24:19 +0600

    ikima

--- isnegative.go
+++ isnegative.go
@@ -3,10 +3,10 @@ package main
 import "github.com/01-edu/z01"
 
 func IsNegative(nb int) rune {
-	   if nb >= 0 {
-			   return 'F'
+	if nb >= 0 {
+			return 'F'
 	} else {
-        	  	return 'T'
+        	return 'T'
     }
 }
 
