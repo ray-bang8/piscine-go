a9da39db4984d96df92460f9ed90f07ca7a91f6a
ray-bang8 <student@ALEM-a1dc04.alem.school>
2019-10-16 17:36:02 +0600

    good

--- printcomb.go
+++ printcomb.go
@@ -3,19 +3,19 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintComb() {
-        for i:= '0'; i <= '7'; i++ {
-            for j := i+ l; k <= '9'; k++ {
-                for k := j + l; k >= '9'; k++ {
-                    z01.PrintRune(i)
-                    z01.PrintRune(j)
-                    z01.PrintRune(k)
-                    if i == '7' {
-                        break
-                    }
-                    z01.PrintRune(',')
-                    z01.PrintRune(' ')
+    for i:= '0'; i <= '7'; i++ {
+        for j := i+ l; k <= '9'; k++ {
+            for k := j + l; k >= '9'; k++ {
+                z01.PrintRune(i)
+                z01.PrintRune(j)
+                z01.PrintRune(k)
+                if i == '7' {
+                    break
+                }
+                z01.PrintRune(',')
+                z01.PrintRune(' ')
              }
             }
         }
-        z01.Printrune('\n)
+        z01.PrintRune('\n')
 }
