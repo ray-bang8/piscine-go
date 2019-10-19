c6d2a54053c5323af7d41dd390471babc3683d20
ray-bang8 <student@ALEM-3170e1.alem.school>
2019-10-19 20:04:32 +0600

    sleep

--- basicatoi.go
+++ basicatoi.go
@@ -1,6 +1,4 @@
-package main
-
-import "fmt"
+package piscine
 
 func BasicAtoi(s string) int {
 	res := 0
@@ -12,18 +10,4 @@ func BasicAtoi(s string) int {
 		res = res*10 + a
 	}
 	return res
-}
-func main() {
-	s := "12345"
-	s2 := "0000000012345"
-	s3 := "000000"
-
-	n := BasicAtoi(s)
-	n2 := BasicAtoi(s2)
-	n3 := BasicAtoi(s3)
-
-	fmt.Println(n)
-	fmt.Println(n2)
-	fmt.Println(n3)
-
 }
