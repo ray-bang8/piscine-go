a981f5f5f3cae0c6087cb4be2e442e0f1e651713
ray-bang8 <student@ALEM-a1dc04.alem.school>
2019-10-21 19:36:59 +0600

    pox

---
+++ recursivepower.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+)
+
+func RecursivePower(nb int, power int) int {
+	if power < 0 || power > 20 {
+		return 1
+	}	
+	if power == 0 {
+			return 1
+		} else if power > 0 {
+				return nb * RecursivePower(nb, power - 1)
+			}
+			return nb	
+		}
+		
+func main() {
+arg1 := 5
+arg2 := 2
+fmt.Println(RecursivePower(arg1, arg2))
+		}
