1bfaf2002f8175ba91561e2b4009ec680b34f4b3
ray-bang8 <student@ALEM-a1dc04.alem.school>
2019-10-21 20:56:24 +0600

    bo

--- fibonacci.go
+++ fibonacci.go
@@ -3,11 +3,9 @@ package piscine
 func Fibonacci(index int) int {
 	if index == 0 {
 		return 0
+	} else if index == 1 {
+		return 1
 	} else {
-		if index == 1 {
-			return 1
-		} else {
-			return Fibonacci(index-1) + Fibonacci(index-2)
-		}
+		return Fibonacci(index-1) + Fibonacci(index-2)
 	}
 }
