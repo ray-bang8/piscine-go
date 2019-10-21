26774fd21f16eb46f0813de227ec1ce495e2345b
ray-bang8 <student@ALEM-a1dc04.alem.school>
2019-10-21 21:55:55 +0600

    whole

--- sqrt.go
+++ sqrt.go
@@ -7,13 +7,11 @@ func Sqrt(nb int) int {
 	if nb <= 0 {
 		return 0
 	}
-	for i := 1; i <= nb/2 ; i++ {
-		if i * i == nb {
+	for i := 1; i <= nb/2; i++ {
+		if i*i == nb {
 			return i
 		}
 	}
 	return 0
-		
-	
+
 }
-	
