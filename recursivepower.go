24ba2b336cd0c17b3759029cb490aa9baabcdf22
ray-bang8 <student@ALEM-a1dc04.alem.school>
2019-10-21 19:38:10 +0600

    pox

--- recursivepower.go
+++ recursivepower.go
@@ -7,17 +7,17 @@ import (
 func RecursivePower(nb int, power int) int {
 	if power < 0 || power > 20 {
 		return 1
-	}	
+	}
 	if power == 0 {
-			return 1
-		} else if power > 0 {
-				return nb * RecursivePower(nb, power - 1)
-			}
-			return nb	
-		}
-		
+		return 1
+	} else if power > 0 {
+		return nb * RecursivePower(nb, power-1)
+	}
+	return nb
+}
+
 func main() {
-arg1 := 5
-arg2 := 2
-fmt.Println(RecursivePower(arg1, arg2))
-		}
+	arg1 := 5
+	arg2 := 2
+	fmt.Println(RecursivePower(arg1, arg2))
+}
